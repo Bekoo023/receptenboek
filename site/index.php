cae747a54414f6faca6c3aff40f486c106772c6c
Bekir Sezgin <162413@novacollege.nl>
2023-03-24 10:56:28 +0100

    begin1

--- site/index.php
+++ site/index.php
@@ -1,5 +1,40 @@
 <?php
 
-// Dit is het startpunt van je applicatie.
 
-echo 'Welkom bij het receptenboek';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Colombiaans eten</title>
+</head>
+
+<body>
+    <div class="bovenheader">
+        <p class="bovenheadertext">Colombiaans eten is de 24ste lekkerste eten van de wereld.</p>
+        <p class="bovenheadertext">Colombiaans eten is buiten Colombia het bekendst in Amerika.</p>
+    </div>
+    <header>
+        <h1>Colombiaans eten.</h1>
+    </header>
+    <nav>
+        <div class="navbar">
+          <a href="index.php">Home</a>
+          <a href="geschiedenis.php">Geschiedenis</a>
+          <a href="index.php">Home</a>
+        </div>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
